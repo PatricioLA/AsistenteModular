eb10732fce889a72bdab8e4545099b26295c7de9
PatricioLA <47235302+PatricioLA@users.noreply.github.com>
2023-02-02 20:57:08 -0600

    Add files via upload
    Uso básico de actions para la validación de nombres

--- Hellorasa/actions/actions.py
+++ Hellorasa/actions/actions.py
@@ -25,3 +25,56 @@
 #         dispatcher.utter_message(text="Hello World!")
 #
 #         return []
+
+from typing import Text, List, Any, Dict
+
+from rasa_sdk import Tracker, FormValidationAction
+from rasa_sdk.events import EventType
+from rasa_sdk.executor import CollectingDispatcher
+from rasa_sdk.types import DomainDict
+
+
+def clean_name(name):
+    return "".join([c for c in name if c.isalpha()])
+
+class ValidateReportForm(FormValidationAction):
+    def name(self) -> Text:
+        return "validate_report_form"
+
+    def validate_first_name(
+        self,
+        slot_value: Any,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: DomainDict,
+    ) -> Dict[Text, Any]:
+        """Validate `first_name` value."""
+
+        # If the name is super short, it might be wrong.
+        name = clean_name(slot_value)
+        if len(name) < 3:
+            dispatcher.utter_message(text="That must've been a typo.")
+            return {"first_name": None}
+        dispatcher.utter_message(text="Muy bien!")
+        return {"first_name": name}
+
+    def validate_last_name(
+        self,
+        slot_value: Any,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: DomainDict,
+    ) -> Dict[Text, Any]:
+        """Validate `last_name` value."""
+
+        # If the name is super short, it might be wrong.
+        name = clean_name(slot_value)
+        if len(name) < 3:
+            dispatcher.utter_message(text="That must've been a typo.")
+            return {"last_name": None}
+        
+        first_name = tracker.get_slot("first_name")
+        if len(first_name) + len(name) < 6:
+            dispatcher.utter_message(text="That's a very short name. We fear a typo. Restarting!")
+            return {"first_name": None, "last_name": None}
+        return {"last_name": name}
